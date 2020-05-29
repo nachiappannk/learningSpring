b4a2d727806484bd6ba31ea132aa4f52d401be5e
Nachiappan NK <nachiappan.nk@mdoc.one>
2020-05-29 22:58:36 +0530

    Starting to learn dependency injection in spring

---
+++ learning-di-in-spring/src/main/java/com/nachiappan/learningdiinspring/LearningDiInSpringApplication.java
@@ -0,0 +1,13 @@
+package com.nachiappan.learningdiinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningDiInSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningDiInSpringApplication.class, args);
+	}
+
+}
